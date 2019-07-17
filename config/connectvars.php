1e2152bed296c594a0960bc3e13f88e35dd6995f
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2019-07-17 14:56:17 +0800

    added initial admin when first start

--- config/connectvars.php
+++ config/connectvars.php
@@ -13,4 +13,18 @@
     if($link === false){
         die("ERROR: Could not connect. " . mysqli_connect_error());
     }
+
+    $check_admin = "SELECT * FROM users WHERE username = admin";
+
+    $result = mysqli_query($link, $check_admin);
+
+    if(mysqli_num_rows($result) === 0){
+        $admin_password = password_hash('Admin123', PASSWORD_DEFAULT);
+        $create_admin = "INSERT INTO users (username, password, department, role) VALUES ('admin', $admin_password, 'IT', 'admin')";
+
+        mysqli_query($conn, $sql);
+    }
+
+    // Close connection
+    mysqli_close($link);
 ?>
